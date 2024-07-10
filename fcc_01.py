a5751f56ede912214d2dc95ec5ff11b80e822eb3
Andriy <andygrigs88@gmail.com>
2024-07-10 22:10:02 +0200

    +

--- fcc_01.py
+++ fcc_01.py
@@ -1,7 +1,25 @@
-text = 'Hello World'
-shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-index = alphabet.find(text[0].lower())
-print(index)
-shifted = alphabet[index + shift]
-print(shifted)
+text = 'Hello Zairay'
+custom_key = 'python'
+
+def vigenere(message, key):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+        if char == ' ':
+            encrypted_text += char
+        else:
+            key_char = key[key_index % len(key)]
+            key_index += 1
+            # Define the offset and the encrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    return encrypted_text
+
+encryption = vigenere(text, custom_key)
+print(encryption)
+
+
